28cd2c276806e593e045997832b951d2173f2bfa
pluem2547 <nhontawat@gmail.com>
2024-03-10 16:57:43 +0700

    last fix

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -7,27 +7,27 @@ function Portfolio() {
       <h3 className={styles.port_title}>Portfolio</h3>
       <div className={styles.port_list}>
         <div className={styles.port_items}>
-            <img src="/src/img/883410_110270602494411_1424649652_o.JPG" alt="" />
+            <img src="https://scontent.fbkk5-5.fna.fbcdn.net/v/t31.18172-8/883410_110270602494411_1424649652_o.jpg?_nc_cat=104&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeF2-QdR8V3Ol5CSxQbI6IgYmYTWT85iIp-ZhNZPzmIiny6z9GiNRXCxhwVrMmzknKF2aa7DtiZ_uMpQx1MHq7_w&_nc_ohc=TNR0ySpFs6kAX--rc57&_nc_ht=scontent.fbkk5-5.fna&cb_e2o_trans=t&oh=00_AfCRTTe1i74vfmnEMnIfluGYVzB5nK2FKSxDDY0LY2oANw&oe=66150AF4" alt="" />
             <p>Me in 1st grade</p>
         </div>
         <div className={styles.port_items}>
-            <img src="./src/img/1836681_238033256384811_1729964908_o.JPG" alt="" />
+            <img src="https://scontent.fbkk5-5.fna.fbcdn.net/v/t31.18172-8/1836681_238033256384811_1729964908_o.jpg?_nc_cat=104&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeGu6sx077nLlLfJEQRPqMaicGMwwI-x-S9wYzDAj7H5L7mJ5YcoaqLB3rUgSG15UQWGoiIw1Spu__fflIhpbO6k&_nc_ohc=qkHr_9xcxOAAX_CYXk2&_nc_ht=scontent.fbkk5-5.fna&cb_e2o_trans=t&oh=00_AfBYhjBozVladwV0Com0PrjQRupavTzESwTCjeARUckraA&oe=6614F67D" alt="" />
             <p>I was in 5th grade.</p>
         </div>
         <div className={styles.port_items}>
-            <img src="./src/img/1961042_236680096520127_1057644146_o.JPG" alt="" />
+            <img src="https://scontent.fbkk5-4.fna.fbcdn.net/v/t31.18172-8/1961042_236680096520127_1057644146_o.jpg?_nc_cat=103&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeGYBgiKRPMopTnafSl_NwWj8aGXezvDhXzxoZd7O8OFfEIc_brEJRr8FotHmegm4PV4PhHNdzdmHWPaA54B-pNc&_nc_ohc=6UlcbJoaMogAX9o_6wu&_nc_oc=AQkTaS-inJc-j-vG4m6Z87AznEunv7ENHOYywodHdqLTxHV4r6ViBrH7VGpRg35KY9yVJHy0-s2RjBHlHA0nQ1OP&_nc_ht=scontent.fbkk5-4.fna&cb_e2o_trans=t&oh=00_AfCiUsur4xp82Ten6fTKWgwCC0QFD07u-jV0HAGGLCn5Lg&oe=6614FE1A" alt="" />
             <p>I was in 6th grade.</p>
         </div>
         <div className={styles.port_items}>
-            <img src="./src/img/12565627_475489209305880_8865719494274402891_n.JPG" alt="" />
+            <img src="https://scontent.fbkk5-3.fna.fbcdn.net/v/t1.18169-9/12565627_475489209305880_8865719494274402891_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeEn1uSc9aeUQoVjjtYgxiemQfD7a3oLQnxB8PtregtCfJqhzWULxUTmowsiNWHH30qcJx4zRbOFBrBTwpZ6bJYP&_nc_ohc=SRZLYNNAXb4AX8GR--u&_nc_ht=scontent.fbkk5-3.fna&cb_e2o_trans=t&oh=00_AfAHqSQNItYhQxZdy-7fd9I7U3R_L7-7kuMbeqRLuvbeUw&oe=661501AD" alt="" />
             <p>A-Math competition</p>
         </div>
         <div className={styles.port_items}>
-            <img src="./src/img/49318386_1060286980826097_1022831029724905472_n.JPG" alt="" />
+            <img src="https://scontent.fbkk5-5.fna.fbcdn.net/v/t1.6435-9/49318386_1060286980826097_1022831029724905472_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeHR0HgdQwILdColmFSkXaA6_zOqsKuWpBT_M6qwq5akFEWvuvDvm1HpT-PgmVwoRvXKyaKPsI1c49AzHYtbfAfZ&_nc_ohc=6aojK2HM1ckAX-M0-LV&_nc_ht=scontent.fbkk5-5.fna&cb_e2o_trans=t&oh=00_AfAYF-oRSx_Api2FraL215IbPvcqSIMNL-hCHYo3_gkGbQ&oe=6614FD03" alt="" />
             <p>I'm in middle school</p>
         </div>
         <div className={styles.port_items}>
-            <img src="./src/img/135280089_1678040562384066_2875571375064484474_n.JPG" alt="" />
+            <img src="https://scontent.fbkk5-6.fna.fbcdn.net/v/t1.6435-9/135280089_1678040562384066_2875571375064484474_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeGhFjobijHyG5LO0UMzIC2JBNxeqIlitsoE3F6oiWK2ytM82pl_AwTLIl9GWDh2yyCrUMuoYfKeesdtC1CmnXXb&_nc_ohc=R_982W3jfCEAX-4WeCC&_nc_ht=scontent.fbkk5-6.fna&cb_e2o_trans=t&oh=00_AfCuUOIUVqTg3MTJfOJZWZAU8Wr2tDPblekXRlbqEGOTsQ&oe=6614FFE6" alt="" />
             <p>I'm in high school</p>
         </div>
       </div>
--- src/components/Testimonials/Testimonials.jsx
+++ src/components/Testimonials/Testimonials.jsx
@@ -14,7 +14,7 @@ function Testimonials() {
             nesciunt!
           </p>
           <img
-            src="./src/img/317617591_3243579789288909_7806397191761817869_n.JPG"
+            src="https://scontent.fbkk5-5.fna.fbcdn.net/v/t39.30808-6/317617591_3243579789288909_7806397191761817869_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeE0F9hoOa3J3HlcYifcvHUCQ2u2-qN6eXpDa7b6o3p5epf_1IMkK7kIX7VvCEykreJbW2ENFAEFOB9EOLC4bZBo&_nc_ohc=44A2KS0ZeI8AX-yxpQm&_nc_ht=scontent.fbkk5-5.fna&cb_e2o_trans=t&oh=00_AfALBlDH2jMKHfmUkagu1sTRp_XRMprzoD9UVYXcRPt0jQ&oe=65F20CC8"
             alt=""
           />
           <h4>Yean</h4>
@@ -28,7 +28,7 @@ function Testimonials() {
             nesciunt!
           </p>
           <img
-            src="./src/img/56990227_550742585416296_2216749796763369472_n.JPG"
+            src="https://scontent.fbkk5-3.fna.fbcdn.net/v/t1.6435-9/56990227_550742585416296_2216749796763369472_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeHTIJu9dLZC_VooI4dZjJDuHSlq9TVd4rodKWr1NV3iug1eNuBarHCgKeWvWPFl5ceXTdfSxI28Nq5Wpp_JAzQe&_nc_ohc=jiUdt-uZB4YAX8L20J_&_nc_ht=scontent.fbkk5-3.fna&cb_e2o_trans=t&oh=00_AfAbkf1tv-j4FPJhlWCByLNKTa-FQrjOCGXH3-JQJv6Q8A&oe=6614D9EB"
             alt=""
           />
           <h4>Game</h4>
@@ -42,7 +42,7 @@ function Testimonials() {
             nesciunt!
           </p>
           <img
-            src="./src/img/131930204_105736114759704_5514231967663578488_n.JPG"
+            src="https://scontent.fbkk5-3.fna.fbcdn.net/v/t1.6435-9/131930204_105736114759704_5514231967663578488_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeFPFv3NUEGzuqNdiJxqBkipGHvrm8KQig4Ye-ubwpCKDl6sHMYb69T22y6vsLZR7V0F01BV96IpJver3pBU9643&_nc_ohc=AxGdTGWnbeIAX-feOZI&_nc_ht=scontent.fbkk5-3.fna&cb_e2o_trans=t&oh=00_AfACSwlM3Z5WX29B7n5XqDbr2RjR9VBGXJ3Zi6wlmIVCJA&oe=6614DBA5"
             alt=""
           />
           <h4>Hood</h4>
